fd827e692b3cec86be2b1665fe2119dc1663d7df
graytrann <trankhaiphong987@gmail.com>
2024-05-02 14:43:51 +0700

    feat: add search input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css"; // icons
+import "primereact/resources/primereact.css"; // core css
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
